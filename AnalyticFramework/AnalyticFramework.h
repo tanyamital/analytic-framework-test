77f0b033b929977706008afa93f4eeb944d43eee
Tanya Mital <tanya.mital@nasdaq.com>
2021-06-08 19:55:00 +0530

    Initial Commit

---
+++ AnalyticFramework/AnalyticFramework.h
@@ -0,0 +1,18 @@
+//
+//  AnalyticFramework.h
+//  AnalyticFramework
+//
+//  Created by Tanya Mital on 08/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnalyticFramework.
+FOUNDATION_EXPORT double AnalyticFrameworkVersionNumber;
+
+//! Project version string for AnalyticFramework.
+FOUNDATION_EXPORT const unsigned char AnalyticFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalyticFramework/PublicHeader.h>
+
+
