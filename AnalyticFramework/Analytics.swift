275b3d3fe9de273729eb8097c6b8d2eada5fcd88
Tanya Mital <tanya.mital@nasdaq.com>
2021-06-09 22:11:01 +0530

    Added my project

---
+++ AnalyticFramework/Analytics.swift
@@ -0,0 +1,16 @@
+//
+//  Analytics.swift
+//  AnalyticFramework
+//
+//  Created by Tanya Mital on 08/06/21.
+//
+
+import Foundation
+
+public class Analytics {
+    public init() {}
+    
+    public func log(message: String) {
+        print("Log message: ",message)
+    }
+}
